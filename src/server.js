6a30648bd5ebc32dd52bc6ecc1c071390aaf674e
dev-chloe <chloekim.4969@gmail.com>
2022-03-31 22:53:35 +0900

    chat with websokets(2)

--- src/server.js
+++ src/server.js
@@ -16,13 +16,29 @@ const handleListen = () => console.log(`Listening on http:localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const sockets = [];
+
 wss.on("connection", (socket) => {
+  sockets.push(socket);
+  socket["nickname"] = "Anon";
   console.log("Connected to Browser");
   socket.on("close", () => console.log("Disconnected from the Browser"));
-  socket.on("message", (message) => {
-    console.log(message.toString("utf8"));
+  socket.on("message", (msg) => {
+    const message = JSON.parse(msg.toString("utf8"));
+    switch (message.type) {
+      case "new_message":
+        sockets.forEach(aSocket => aSocket.send(`${socket.nickname}: ${message.payload.toString("utf8")}`));
+        break;
+      case "nickname":
+        socket["nickname"] = message.payload;
+        break;
+    }
+    // if (message.type === "new_message") {
+    //   sockets.forEach(aSocket => aSocket.send(message.payload.toString("utf8")));
+    // } else if(message.type === "nickname") {
+    //   console.log(message.payload.toString("utf8"))
+    // }
   });
-  socket.send("hi");
 });
 
 server.listen(3000, handleListen);
